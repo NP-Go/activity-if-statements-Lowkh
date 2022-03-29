007eee464af9aabe9b3871005a589db340d93f47
ShortCoursesICT <ict.cet.shortcourses@gmail.com>
2022-03-29 11:20:30 +0800

    added some if comparison

--- if.go
+++ if.go
@@ -1,10 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	number1 := 17
 	number2 := 24
 	resultMessage := "No outcome."
 	//Insert your code here
 	//Hint: You may wish to make use of strconv.Itoa to convert int to string
-
+	if number1 < number2 {
+		resultMessage = "24 is bigger than 17"
+		fmt.Println(resultMessage)
+	} else {
+		fmt.Println("Numbers not found!!")
+	}
+	if number1 == number2 {
+		fmt.Println("24 is bigger than 17")
+	}
 }
